8d658ecbf4056f93969311a0b7f9e34530e73e57
Michael <79370938+DwMichael@users.noreply.github.com>
2024-06-18 11:45:19 +0200

    Update tmdb_api.dart

--- flixo_app/lib/api/tmdb_api.dart
+++ flixo_app/lib/api/tmdb_api.dart
@@ -1,13 +1,12 @@
 import 'package:tmdb_api/tmdb_api.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../model/cast.dart';
 import '../model/genre.dart';
 import '../model/movie.dart';
 
 class TmdbApi {
-  final String apiKey = 'b36e3e21d0e7736e57a105307d79ac1f';
-  final String readaccesstoken =
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzZlM2UyMWQwZTc3MzZlNTdhMTA1MzA3ZDc5YWMxZiIsInN1YiI6IjY0NmZjMzJlMzM2ZTAxMDEyYWNmMWExMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Oebk-k_Tc1V0j1T5m_eJ5Q2Fv7gKs2jgkB5Hk0vfGFk';
+  final String apiKey = dotenv.env['API_KEY']!;;
+  final String readaccesstoken = dotenv.env['READ_ACCESS_TOKEN']!;
 
   Future<List<Cast>> getCastList(int movieId) async {
     try {
